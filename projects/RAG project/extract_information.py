23a16920c8bb5b13eec2330d236c03a86310e289
Anton Tveito <anton_tveito@dhcp-10-22-51-72.wlan.ntnu.no>
2024-05-02 21:35:33 +0200

    Added new versions of libraries

--- projects/RAG project/extract_information.py
+++ projects/RAG project/extract_information.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import MongoDBAtlasVectorSearch
-from langchain.document_loaders import DirectoryLoader
-from langchain.llms import OpenAI
-from langchain.chains import RetrievalQA
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import MongoDBAtlasVectorSearch
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.llms import openai
+from langchain.chains.retrieval_qa.base import RetrievalQA
 import gradio as gr
 from gradio.themes.base import Base
 import key_params
